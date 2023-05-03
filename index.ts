43247380d91837875b6ef010f84e15276b106428
canhotogremista <132523491+canhotogremista@users.noreply.github.com>
2023-05-03 20:54:15 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+// Import stylesheets
+import './style.css';
+
+console.log('Olá mundo');
+
+let xy: number = -10;
+console.log('O valor de xy é ' + xy);
+
+let str: string = 'texto ' + xy;
+console.log('str - ' + str);
+
+
