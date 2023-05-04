0b3760ab8e29b08aa52840c80a000deba2d15c66
canhotogremista <132523491+canhotogremista@users.noreply.github.com>
2023-05-03 21:30:55 -0300

    2 files modified

--- index.ts
+++ index.ts
@@ -15,3 +15,44 @@ function getNome(pessoa: { nome: string; sobrenome: string }): string {
 }
 
 console.log('O nome é ' + getNome({ nome: 'José', sobrenome: 'Silva' }));
+
+document.getElementById('btn_pi').addEventListener('click', () => {
+  let numero: number = Number(
+    (<HTMLInputElement>document.getElementById('parimpar_input')).value
+  );
+  if (numero % 2 == 0) {
+    document.getElementById('resultado_pi').innerHTML = 'Par';
+  } else {
+    document.getElementById('resultado_pi').innerHTML = 'Impar';
+  }
+});
+
+document.getElementById('btn_tabu').addEventListener('click', () => {
+  let numero: number = Number(
+    (<HTMLInputElement>document.getElementById('tabuada_input')).value
+  );
+  const tabela = document.getElementById('resultado_tabu');
+  tabela.innerHTML = '';
+  for (let i = 1; i <= 10; i++) {
+    const linha = tabela.insertRow();
+    const cel1 = linha.insertCell();
+    const cel2 = linha.insertCell();
+    cel1.innerHTML = `${numero} x ${i} =`;
+    cel2.innerHTML = numero * i;
+  }
+});
+
+document.getElementById('btn_ale').addEventListener('click', () => {
+  let numero: number = Number(
+    (<HTMLInputElement>document.getElementById('ale_input')).value
+  );
+  const tabela = document.getElementById('resultado_ale');
+  tabela.innerHTML = '';
+  for (let i = 1; i <= 10; i++) {
+    const linha = tabela.insertRow();
+    const cel1 = linha.insertCell();
+    const cel2 = linha.insertCell();
+    cel1.innerHTML = `${i} =`;
+    cel2.innerHTML = Math.trunc(Math.random() * numero);
+  }
+});
