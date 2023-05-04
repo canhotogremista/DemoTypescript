07281ba9f5d783d9bac7d1cd827d1c23db70c779
canhotogremista <132523491+canhotogremista@users.noreply.github.com>
2023-05-03 21:03:06 -0300

    1 file modified

--- index.ts
+++ index.ts
@@ -9,4 +9,9 @@ console.log('O valor de xy é ' + xy);
 let str: string = 'texto ' + xy;
 console.log('str - ' + str);
 
+function getNome(pessoa: { nome: string; sobrenome: string }): string {
+  //return pessoa.nome + ' ' + pessoa.sobrenome;
+  return `${pessoa.nome} ${pessoa.sobrenome}`;
+}
 
+console.log('O nome é ' + getNome({ nome: 'José', sobrenome: 'Silva' }));
